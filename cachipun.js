866910940d2459d6183c1a3a96ce0e722702cf02
Pia <insside.pia@hotmail.com>
2022-01-21 15:39:03 -0300

    Desafio Cachipun 3

--- cachipun.js
+++ cachipun.js
@@ -1,24 +1,61 @@
 // veces que desea jugar
 // que opcion elige
 
+const n1 = document.getElementById("player1");
 
-let opcion = prompt(`
-Elija una opción:
-1. Piedra
-2. Papel
-3. Tijera
-`);
 
-switch (opcion) {
-    case "1":
-        console.log("Piedra");
-        break;
-    case "2":
-        console.log("Papel");
-        break;
-    case "3":
-        console.log("Tijera");
-        break;
+// que le sale al pc
+
+let n2;
+
+function getRandom() {
+    return Math.floor(Math.random() * 3);
 }
 
-// Math.floor(Math.random() * 3)
+if (getRandom == 0) {
+    n2 = "papel"
+} else if (getRandom == 1) {
+    n2 = "tijeras"
+} else {
+    n2 = "piedra";
+
+}
+
+console.log(n2);
+
+
+
+// comparar resultados
+// mostrar ganador
+// repetir si es necesario
+
+// let veces = prompt(`
+// ¿Cuantas veces desea jugar?
+// `);
+
+// let n1 = ["Piedra", "Papel", "Tijera"];
+// let n1 = [0, 1, 2];
+// let n2;
+
+// let n1 = prompt(`
+// Elija una opción:
+// 1. Piedra
+// 2. Papel
+// 3. Tijera
+// `);
+
+// switch (n1) {
+//     case "1":
+//         console.log("Piedra");
+//         break;
+//     case "2":
+//         console.log("Papel");
+//         break;
+//     case "3":
+//         console.log("Tijera");
+//         break;
+// }
+
+// function comparar(resultado1, resultado2) {
+//     return parseInt(n1)
+// }
