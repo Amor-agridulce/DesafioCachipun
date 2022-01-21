46692e9dbcf6181c21ca36ac72783acca7430542
Pia <insside.pia@hotmail.com>
2022-01-21 14:38:43 -0300

    Desafio Cachipun 2

--- cachipun.js
+++ cachipun.js
@@ -1,1 +1,24 @@
-Math.floor(Math.random() * 3)
+// veces que desea jugar
+// que opcion elige
+
+
+let opcion = prompt(`
+Elija una opción:
+1. Piedra
+2. Papel
+3. Tijera
+`);
+
+switch (opcion) {
+    case "1":
+        console.log("Piedra");
+        break;
+    case "2":
+        console.log("Papel");
+        break;
+    case "3":
+        console.log("Tijera");
+        break;
+}
+
+// Math.floor(Math.random() * 3)
